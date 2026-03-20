3d8f491438fc092013bcecfa76005f5a14dcfe36
Dreampop0215 <ShadowFarewell0711@outlook.com>
2026-03-20 16:28:38 +0800

    Update HomePage

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -26,7 +26,7 @@ const HomePage: React.FC = () => {
                 {/*    这是占位内容，未来可以在这里添加欢迎信息或其他内容。*/}
                 {/*</p>*/}
                 <img
-                    src="/public/images/watcher.jpg"
+                    src="/images/watcher.jpg"
                     alt="watcher"
                     style={{
                         maxWidth: "100%",
